5757d2404d4bcb52742ce31d851d7cf1237e7669
milleymilli <milleyny@gmail.com>
2025-12-03 11:14:19 +0000

    putting the files in the right directories

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
