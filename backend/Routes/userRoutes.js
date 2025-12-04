438abe19088d5492710a88eb4abc8654a214b472
Slayer0206 <andrewpratten06@gmail.com>
2025-12-04 20:58:43 +0000

    Add signup and signin routes
    just 2 lines of code adding routes for signin and signup

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -5,5 +5,6 @@ const { getUsers, createUser } = require("../Controllers/userController");
 
 // Map the URL '/' to the functions
 router.route("/").get(getUsers).post(createUser);
-
+router.post('/signup', signup);
+router.post('/signin', signin);
 module.exports = router;
